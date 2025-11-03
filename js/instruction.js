d7fc620af59aca2f4a039070b73c00439444247d
Allysa <Alna0003@stud.ek.dk>
2025-11-03 16:38:51 +0100

    inputfelter

--- js/instruction.js
+++ js/instruction.js
@@ -4,14 +4,26 @@ document.querySelector("#softspots").addEventListener("click", softspotsF);
 
 function softspotsF() {
   console.log("softspotsF button clicked");
+  document.querySelector(".info-text h2").innerText = "Softspots";
+  document.querySelector(".info-text p").innerText =
+    "Her får du mere viden omkring Dragens softspots og hvordan du bedst håndterer dem i en nødsituation.";
 }
-document.querySelector("#vinger").addEventListener("click", vingerF);
 
-function vingerF() {
-  console.log("vingerF button clicked");
+// -------------------------------
+document.querySelector("#dragen").addEventListener("click", dragenF);
+
+function dragenF() {
+  console.log("dragenF button clicked");
+  document.querySelector(".info-text h2").innerText = "Om dragen";
+  document.querySelector(".info-text p").innerText =
+    "Her får du mere viden omkring Dragen.";
 }
+// -------------------------------
 document.querySelector("#mave").addEventListener("click", maveF);
 
 function maveF() {
   console.log("maveF button clicked");
+  document.querySelector(".info-text h2").innerText = "Mave";
+  document.querySelector(".info-text p").innerText =
+    "Her får du mere viden omkring Dragens mave og hvordan du bedst håndterer den i en nødsituation.";
 }
