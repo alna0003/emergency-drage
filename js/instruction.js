f65e04a7efb17875e06bec889fd17363f93f6520
Allysa <Alna0003@stud.ek.dk>
2025-11-11 08:40:40 +0100

    lavet register

--- js/instruction.js
+++ js/instruction.js
@@ -19,11 +19,11 @@ function dragenF() {
     "Her får du mere viden omkring Dragen.";
 }
 // -------------------------------
-document.querySelector("#mave").addEventListener("click", maveF);
+document.querySelector("#historie").addEventListener("click", historieF);
 
-function maveF() {
-  console.log("maveF button clicked");
-  document.querySelector(".info-text h2").innerText = "Mave";
+function historieF() {
+  console.log("historieF button clicked");
+  document.querySelector(".info-text h2").innerText = "Historie";
   document.querySelector(".info-text p").innerText =
-    "Her får du mere viden omkring Dragens mave og hvordan du bedst håndterer den i en nødsituation.";
+    "Der var engang en majestætisk drage, der vågnede op fra sin ældgamle hvile dybt i det ukendte univers. På mystisk vis blev den trukket gennem stjernerne og ankom til vores planet, hvor den blev tiltrukket af den smukke natur og de fredelige landskaber. Men denne drage var ikke som andre drager – den var magisk og også meget følsom. Hvis nogen vækkede dens vrede eller irriterede den, reagerede den med kraftfuld ild – ikke af ondskab, men som en beskyttelse af sit nye hjem. Denne ild kunne skabe dramatiske naturkatastrofer, som store brander og rasende floder, men kun når dragen var ked af det eller følte sig truet. De mennesker, der lærte at respektere og tale blidt til dragens hjerte, oplevede, at den blev venlig og beskyttende, og den viste dem, at den blot var en magisk vogter, der ønskede fred. Og fra da af levede dragen og menneskene i harmoni, hvor kærlighed og respekt forhindrede dens vrede, og naturen blomstrede under dens vogterblik.";
 }
